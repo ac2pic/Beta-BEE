22ae4e172e11dfbf9b8c8f915a2498baa78c3f93
ac2pic <ac2pic@gmail.com>
2019-11-10 14:22:09 -0600

    Certain party members are now playable

---
+++ beta-bee/postload.js
@@ -0,0 +1,1 @@
+sc.PLAYABLE_OPTIONS = ["Emilie", "Lea",  "Qine"];
--- beta-bee/prestart.js
+++ beta-bee/prestart.js
@@ -1,1 +1,9 @@
-console.log('I exist.');
+import "./js/bee/playable/playable.js";
+
+import "./js/bee/player/player-model.js";
+
+import "./js/bee/playable/controls.js";
+
+import "./js/bee/party/party.js";
+
+import "./js/bee/inject/hp-hud.js";
