2d0d74c7f708d36a2a96690af0ae0ce645475ec5
ac2pic <ac2pic@gmail.com>
2019-11-12 00:01:38 -0600

    Reorganized import statements to fix impact module load error

--- beta-bee/prestart.js
+++ beta-bee/prestart.js
@@ -1,13 +1,23 @@
-import "./js/bee/playable/playable.js";
+
+// load all the injects
+
+import "./js/bee/inject/hp-hud.js";
 
 import "./js/bee/player/player-model.js";
+
+// load all that relies on a game module
 
 import "./js/bee/playable/controls.js";
 
 import "./js/bee/party/party.js";
 
-import "./js/bee/inject/hp-hud.js";
 
 import "./js/bee/calendar/calendar.js";
 
 import "./js/bee/calendar/event-steps.js";
+
+// load all that relies on a custom game module
+
+import "./js/bee/playable/playable.js";
+
+import "./js/bee/playable/schedule/schedule.js";
