3cc89c7cbb59c8114508235a2a5a46612d26b9d2
ac2pic <ac2pic@gmail.com>
2019-11-11 18:33:29 -0600

    Fixed error  in ig.Storage

--- beta-bee/js/bee/inject/storage.js
+++ beta-bee/js/bee/inject/storage.js
@@ -1,6 +1,6 @@
 ig.module("bee.inject.storage").requires("impact.feature.storage.storage").defines(function() {
 	
 	ig.Storage.inject({
-		data: new ig.StorageData(window.BEE_DEBUG ? "bb.save.dev", "bb.save")
+		data: new ig.StorageData(window.BEE_DEBUG ? "bb.save.dev" : "bb.save")
 	});
 });
