ed49f073b87a1f043e4bf73e93618f4885903f60
ac2pic <ac2pic@gmail.com>
2019-11-13 09:09:22 -0600

    Added mood modules to prestart

--- beta-bee/prestart.js
+++ beta-bee/prestart.js
@@ -16,10 +16,18 @@ import "./js/bee/calendar/calendar.js";
 
 import "./js/bee/calendar/event-steps.js";
 
+
 // load all that relies on a custom game module
 
 import "./js/bee/playable/playable.js";
 
 import './js/bee/playable/config.js';
 
+import "./js/bee/playable/mood/mood.js";
+
+import "./js/bee/playable/mood/event-steps.js";
+
+
 import "./js/bee/playable/schedule/schedule.js";
+
+
