d254d3ee91f2c107084e65e2c98aecd9a905b1f3
ac2pic <ac2pic@gmail.com>
2019-11-14 22:13:42 -0600

    PlayerSubConfig is now clonable

---
+++ beta-bee/js/bee/inject/player/player-config.js
@@ -0,0 +1,13 @@
+ig.module("bee.inject.player.player-config").requires("game.feature.player.player-config").defines(function() {
+    sc.PlayerSubConfig.inject({
+        cloneCopy: {},
+        init: function(element, actions) {
+            this.cloneCopy = {element, actions};
+            this.parent(element, actions);
+        },
+        copy: function() {
+            const {element, actions} = this.cloneCopy;
+            return new sc.PlayerSubConfig(element, actions);
+        }
+    });
+});
