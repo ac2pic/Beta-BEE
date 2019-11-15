a46a8bbd326648cc7ff8b754ab9a1d3845562e67
ac2pic <ac2pic@gmail.com>
2019-11-14 22:13:18 -0600

    Added TELEPORT_VAR for variable teleport insertions.

---
+++ beta-bee/js/bee/inject/event-steps.js
@@ -0,0 +1,10 @@
+ig.module("bee.inject.event-steps").requires("impact.feature.base.event-steps").defines(function() {
+    ig.EVENT_STEP.TELEPORT_VAR = ig.EVENT_STEP.TELEPORT.extend({
+        start: function() {
+            this.map = ig.Event.getExpressionValue(this.map);
+            this.marker = ig.Event.getExpressionValue(this.marker);
+            this.parent();
+        }
+    });
+});
+
