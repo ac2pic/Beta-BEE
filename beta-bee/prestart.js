b75b77d1d8dedd6809c12c1bbc215628da7e35dd
ac2pic <ac2pic@gmail.com>
2019-11-15 19:24:03 -0600

    Generated prestart.js

--- beta-bee/prestart.js
+++ beta-bee/prestart.js
@@ -1,44 +1,35 @@
+import "./js/game/feature/bee/base/event-steps.js";
 
-// load all the injects
+import "./js/game/feature/bee/calendar/calendar.js";
 
-import "./js/bee/inject/hp-hud.js";
+import "./js/game/feature/bee/calendar/event-steps.js";
 
-import "./js/bee/inject/exp-hud.js";
+import "./js/game/feature/bee/gui/hud/exp-hud.js";
 
-import "./js/bee/player/player-model.js";
+import "./js/game/feature/bee/gui/hud/hp-hud.js";
 
-import "./js/bee/inject/player/player-level.js";
+import "./js/game/feature/bee/party/party.js";
 
-import "./js/bee/inject/player/player-config.js";
+import "./js/game/feature/bee/playable/controls.js";
 
-import "./js/bee/inject/event-steps.js";
+import "./js/game/feature/bee/playable/energy.js";
 
-// load all that relies on nothing
+import "./js/game/feature/bee/playable/mood/mood.js";
 
-import "./js/bee/playable/energy.js";
+import "./js/game/feature/bee/playable/schedule/events.js";
 
-// load all that relies on a game module
+import "./js/game/feature/bee/player/player-config.js";
 
-import "./js/bee/playable/controls.js";
+import "./js/game/feature/bee/player/player-level.js";
 
-import "./js/bee/party/party.js";
+import "./js/game/feature/bee/player/player-model.js";
 
+import "./js/game/feature/bee/skills/skilltree.js";
 
-import "./js/bee/calendar/calendar.js";
+import "./js/game/feature/bee/playable/mood/event-steps.js";
 
-import "./js/bee/calendar/event-steps.js";
+import "./js/game/feature/bee/playable/playable.js";
 
-// load all that relies on a custom game module
+import "./js/game/feature/bee/playable/config.js";
 
-import "./js/bee/playable/playable.js";
-
-import './js/bee/playable/config.js';
-
-import "./js/bee/playable/mood/mood.js";
-
-import "./js/bee/playable/mood/event-steps.js";
-
-
-import "./js/bee/playable/schedule/schedule.js";
-
-
+import "./js/game/feature/bee/playable/schedule/schedule.js";
