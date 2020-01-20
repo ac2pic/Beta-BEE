a82fafd2ae45975c7a72197ae43d2d1174802047
ac2pic <ac2pic@gmail.com>
2020-01-20 11:31:47 -0600

    Moved all general stuff to unified-steps

--- beta-bee/js/game/feature/bee/base/event-steps.js
+++
@@ -1,9 +0,0 @@
-ig.module("game.feature.bee.base.event-steps").requires("impact.feature.base.event-steps").defines(function() {
-    ig.EVENT_STEP.TELEPORT_VAR = ig.EVENT_STEP.TELEPORT.extend({
-        start: function() {
-            this.map = ig.Event.getExpressionValue(this.map);
-            this.marker = ig.Event.getExpressionValue(this.marker);
-            this.parent();
-        }
-    });
-});
