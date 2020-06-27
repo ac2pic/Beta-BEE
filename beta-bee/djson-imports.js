511857a92798723adf4dca2f676d3c9206ea71c8
Node Code Formatter <formatter@1337z.ninja>
2020-06-27 23:28:24 +0000

    Formatted code

--- beta-bee/djson-imports.js
+++ beta-bee/djson-imports.js
@@ -1,6 +1,6 @@
 export default function DjsonImports(mod) {
 
-    DynamicJson.forRegExpUrl(/data\/animations\/class\/(.*).json/, async function (name) {
+    DynamicJson.forRegExpUrl(/data\/animations\/class\/(.*).json/, async function(name) {
         const genericData = await fetch(`/${mod.baseDirectory}assets/data/animations/class/generic.json`).then(resp => resp.json());
         if (name === 'aiko') {
             genericData.namedSheets.walk.offY = 120;
@@ -11,7 +11,7 @@ export default function DjsonImports(mod) {
     });
 
     // assets\data\characters\npc
-    DynamicJson.forRegExpUrl(/data\/characters\/npc\/female([0-9])?.json/, async function (number) {
+    DynamicJson.forRegExpUrl(/data\/characters\/npc\/female([0-9])?.json/, async function(number) {
         return await fetch(`/${mod.baseDirectory}assets/data/characters/npc/female.json`).then(resp => resp.json());
     });
 
