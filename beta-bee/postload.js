afaa05ee011f18bcd851de3e19ce26a447e550e3
ac2pic <ac2pic@gmail.com>
2020-06-28 16:05:32 -0500

    Reverted debug stuff

--- beta-bee/postload.js
+++ beta-bee/postload.js
@@ -1,7 +1,7 @@
 sc.PLAYABLE_OPTIONS = ["Emilie", "Lea", "Glasses"];
 window.BEE_DEBUG = true;
 
-window.LOAD_LEVEL_ON_GAME_START = "emilie.face-test";
+window.LOAD_LEVEL_ON_GAME_START = "real.home.room_emilie_night";
 
 
 window.MARKER_ON_GAME_START = "";
